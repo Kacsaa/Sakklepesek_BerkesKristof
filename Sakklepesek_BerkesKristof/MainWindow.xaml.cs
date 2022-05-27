2c704af3c248d99208b9b59d90410c7b992f57a6
Kristóf_B <jkacsa2004@gmail.com>
2022-05-27 18:12:19 +0200

    befejezés

--- Sakklepesek_BerkesKristof/MainWindow.xaml.cs
+++ Sakklepesek_BerkesKristof/MainWindow.xaml.cs
@@ -16,9 +16,6 @@ using System.IO;
 
 namespace Sakklepesek_BerkesKristof
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         Button[,] mezok = new Button[8,8];
